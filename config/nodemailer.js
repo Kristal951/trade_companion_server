2dae9417ec660d04835f123939edb982ad695b69
Kristal_Dev <kristaldev001@gmail.com>
2025-12-10 14:36:55 +0100

    fixing verification email sending

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -3,7 +3,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "Gmail",
+   host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.NODEMAILER_EMAIL_NAME,
     pass: process.env.NODEMAILER_EMAIL_PASSWD,
